e8fbb4a96636b02fe8e888596237202fbee1ae94
matt <xmatiasp@gmail.com>
2021-04-09 19:58:22 -0300

    Actualizando pruebas

--- tests/pageObjects/registerAccountItems.py
+++ tests/pageObjects/registerAccountItems.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 class RegisterAccountItems:
     def __init__(self, driver):
@@ -12,6 +14,8 @@ class RegisterAccountItems:
         self.suscribe_input = (By.NAME, 'newsletter')
         self.privacy_button = (By.NAME, 'agree')
         self.confirm_button = (By.XPATH, '//*[@id="content"]/form/div/div/input[2]')
+        self.account_confirmation = (By.XPATH, '//*[@id="content"]/h1')
+        self.account_registered = (By.XPATH, '//*[@id="account-register"]/div[1]')
 
     def fill_name_input(self, keys):
         self.driver.find_element(*self.first_name_input).send_keys(keys)
@@ -39,3 +43,11 @@ class RegisterAccountItems:
 
     def click_confirm_button(self):
         self.driver.find_element(*self.confirm_button).click()
+
+    def return_cofirmed_account_text(self):
+        confirmed_account = WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(self.account_confirmation))
+        return confirmed_account.text
+
+    def return_wrong_mail_account_text(self):
+        wrong_mail = WebDriverWait(self.driver, 7).until(EC.presence_of_element_located(self.account_registered))
+        return wrong_mail.text
--- tests/testSignIn.py
+++ tests/testSignIn.py
@@ -1,7 +1,7 @@
 import unittest
 from selenium import webdriver
-from pageObjects.homePageItems import HomePageItems
-from pageObjects.registerAccountItems import RegisterAccountItems
+from tests.pageObjects.homePageItems import HomePageItems
+from tests.pageObjects.registerAccountItems import RegisterAccountItems
 import time
 
 class OpenCartSignInTests(unittest.TestCase):
@@ -13,25 +13,39 @@ class OpenCartSignInTests(unittest.TestCase):
         self.itemsHomePage = HomePageItems(self.driver)
         self.itemsRegisterAcc = RegisterAccountItems(self.driver)
 
-    def test_register_in_opencart(self):
+    def test_register_in_opencart_(self):
         self.itemsHomePage.click_in_myaccount()
         self.itemsHomePage.click_in_register()
         self.itemsRegisterAcc.fill_name_input('Bruce')
         self.itemsRegisterAcc.fill_last_name_input('Wayne')
-        self.itemsRegisterAcc.fill_email_input('batman4@prueba.com')
+        #utilizar nuevo mail cada vez que se corra esta prueba
+        self.itemsRegisterAcc.fill_email_input('batman9@prueba.com')
         self.itemsRegisterAcc.fill_telephone_input('1234666')
         self.itemsRegisterAcc.fill_password_input('123prueba')
         self.itemsRegisterAcc.fill_confirm_password_input('123prueba')
         self.itemsRegisterAcc.click_suscribe_input()
         self.itemsRegisterAcc.click_privacy_button()
         self.itemsRegisterAcc.click_confirm_button()
-        time.sleep(5)
+        self.assertEqual('Your Account Has Been Created!', self.itemsRegisterAcc.return_cofirmed_account_text())
+
+    def test_register_in_opencart_with_account_registered(self):
+        self.itemsHomePage.click_in_myaccount()
+        self.itemsHomePage.click_in_register()
+        self.itemsRegisterAcc.fill_name_input('Bruce')
+        self.itemsRegisterAcc.fill_last_name_input('Wayne')
+        self.itemsRegisterAcc.fill_email_input('batman1@prueba.com')
+        self.itemsRegisterAcc.fill_telephone_input('1234666')
+        self.itemsRegisterAcc.fill_password_input('123prueba')
+        self.itemsRegisterAcc.fill_confirm_password_input('123prueba')
+        self.itemsRegisterAcc.click_suscribe_input()
+        self.itemsRegisterAcc.click_privacy_button()
+        self.itemsRegisterAcc.click_confirm_button()
+        self.assertEqual('Warning: E-Mail Address is already registered!', self.itemsRegisterAcc.return_wrong_mail_account_text())
 
 
     def tearDown(self):
         self.driver.close()
         self.driver.quit()
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     unittest.main()
