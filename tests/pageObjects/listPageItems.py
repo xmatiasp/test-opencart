22bf6842e92ed295671207a5d881d27f9ebfacd6
matt <xmatiasp@gmail.com>
2021-04-14 18:43:22 -0300

    Actualizando

--- tests/pageObjects/listPageItems.py
+++ tests/pageObjects/listPageItems.py
@@ -5,13 +5,16 @@ class ListPageItems:
     def __init__(self, driver):
         self.driver = driver
         self.input_sort = (By.ID, 'input-sort')
+        self.result_text_search = (By.XPATH, '//*[@id="content"]/h1')
 
     def sort_by_price_high_low(self):
-
         input_sort_element = self.driver.find_element(*self.input_sort)
         input_sort = Select(input_sort_element)
         input_sort.select_by_visible_text('Price (High > Low)')
 
+    def return_result_text(self):
+        text_result = self.driver.find_element(*self.result_text_search).text
+        return text_result
 
     def return_products_name_and_price(self):
         products = []
@@ -22,7 +25,9 @@ class ListPageItems:
             product_price = self.driver.find_element_by_xpath(f'//*[@id="content"]/div[3]/div[{i + 1}]/div/div[2]/div[1]/p[2]').text
             price.append(product_price)
 
-        print(products)
-        print(price)
+        products_data = open('products.txt', 'w+')
+        products_data.write(str(products)+'\n')
+        products_data.write(str(price))
+        products_data.close()
 
         return products, price
